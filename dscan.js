4a035e43cca14c7ffe05c7f9079b1cb7d701e054
Douglas Thompson <douglas.a.thompson@gmail.com>
2013-03-29 14:44:47 -0500

    Prototype

---
+++ dscan.js
@@ -0,0 +1,106 @@
+var AU_KM_FACTOR = 149597871,
+    convertAuToKm,
+    convertKmToAu,
+    increaseMin,
+    decreaseMax;
+
+function AuToKm(Au) {
+    "use strict";
+    return Au * AU_KM_FACTOR;
+}
+
+function KmToAu(Km) {
+    "use strict";
+    return Km / AU_KM_FACTOR;
+}
+
+function midpoint(max, min) {
+    "use strict";
+    return (max - min) / 2;
+}
+
+
+function KmConverterView(AuTextBox, KmTextBox) {
+    "use strict";
+    return function() {
+        AuTextBox.value = KmToAu(KmTextBox.value);
+    };
+}
+
+function AuConverterView(AuTextBox, KmTextBox) {
+    "use strict";
+    return function() {
+        KmTextBox.value = AuToKm(AuTextBox.value);
+    };
+}
+
+function MinIncreaser(maxText, minText, testText) {
+    "use strict";
+    return function() {
+        minText.value = midpoint(maxText.value, minText.value) + parseFloat(minText.value);
+        testText.innerHTML = Math.round(parseFloat(minText.value) + midpoint(maxText.value, minText.value));
+    };
+}
+
+function MaxDecreaser(maxText, minText, testText) {
+    "use strict";
+    return function() {
+        maxText.value = maxText.value - midpoint(maxText.value, minText.value);
+        testText.innerHTML = Math.round(parseFloat(minText.value) + midpoint(maxText.value, minText.value));
+    };
+}
+
+
+function initialize() {
+    "use strict";
+    var kmTextBox = document.getElementById("km"),
+        auTextBox = document.getElementById("au"),
+        maxTextBox = document.getElementById("max"),
+        minTextBox = document.getElementById("min"),
+        testText = document.getElementById("test");
+
+
+    convertAuToKm = AuConverterView(auTextBox, kmTextBox);
+    convertKmToAu = KmConverterView(auTextBox, kmTextBox);
+
+    increaseMin = MinIncreaser(maxTextBox, minTextBox, testText);
+    decreaseMax = MaxDecreaser(maxTextBox, minTextBox, testText);
+
+    testText.innerHTML = "test";
+}
+
+var AU_KM_FACTOR = 149597871,
+    convertAuToKm,
+    convertKmToAu,
+    increaseMin,
+    decreaseMax;
+
+function AuToKm(Au) {
+    "use strict";
+    return Au * AU_KM_FACTOR;
+}
+
+function KmToAu(Km) {
+    "use strict";
+    return Km / AU_KM_FACTOR;
+}
+
+function midpoint(max, min) {
+    "use strict";
+    return (max - min) / 2;
+}
+
+
+function KmConverterView(AuTextBox, KmTextBox) {
+    "use strict";
+    return function() {
+        AuTextBox.value = KmToAu(KmTextBox.value);
+    };
+}
+
+function AuConverterView(AuTextBox, KmTextBox) {
+    "use strict";
+    return function() {
+        KmTextBox.value = AuToKm(AuTextBox.value);
+    };
+}
