754da5ba7b539eee61901986affbda7097f03299
Douglas Thompson <douglas.a.thompson@gmail.com>
2015-09-19 15:43:39 -0500

    Updated to most recent version

--- dscan.js
+++ dscan.js
@@ -1,3 +1,17 @@
+var AU_KM_FACTOR = 149597871,
+    MAX_DSCAN_KM = 2139249551, 
+    convertAuToKm,
+    convertKmToAu,
+    increaseMin,
+    decreaseMax,
+    updateRanges,
+    resetSearch,
+		updateTestpoint,
+		setMidpoint1d,
+		setMidpoint2d,
+		setMidpoint3d,
+    undo;
+
 function AuToKm(Au) {
   "use strict";
   return Au * AU_KM_FACTOR;
@@ -8,21 +22,32 @@ function KmToAu(Km) {
   return Km / AU_KM_FACTOR;
 }
 
-function midpoint(max, min) {
+function midpoint1d(max, min) {
   "use strict";
   return min + (max - min) / 2;
 }
 
+function midpoint2d(max, min) {
+	"use strict";
+	return min+(max-min) / Math.pow(2, 1/2);
+
+}
+
+function midpoint3d(max, min) {
+	"use strict";
+	return min+(max-min) / Math.pow(2, 1/3);
+}
+
 function SearchResetter(max, min) {
   return function() {
-    max.value = 2147483647;
+    max.value = MAX_DSCAN_KM;
     min.value = 1;
 
     updateRanges();
   }
 }
 
-function TestpointCalculator(maxTextBox, minTextBox, testText) {
+function TestpointCalculator(maxTextBox, minTextBox, testText, midpoint) {
   return function() {
     var max = parseFloat(maxTextBox.value),
         min = parseFloat(minTextBox.value);
@@ -31,8 +56,9 @@ function TestpointCalculator(maxTextBox, minTextBox, testText) {
   }
 }
 
-function onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView) {
-  var updateTestpoint = TestpointCalculator(maxTextBox, minTextBox, testText);
+function onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView, midpoint) {
+	
+  updateTestpoint = TestpointCalculator(maxTextBox, minTextBox, testText, midpoint);
 
   return function() {
     var max = parseFloat(maxTextBox.value),
@@ -47,7 +73,7 @@ function onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView) {
         var context = topView.getContext("2d");
         var centerX = topView.width / 2;
         var centerY = topView.height / 2;
-        var radius = (max / 2147483647) * (topView.width / 2);
+        var radius = (max / MAX_DSCAN_KM) * (topView.width / 2);
 
         context.clearRect(0, 0, topView.width, topView.height);
 
@@ -56,34 +82,38 @@ function onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView) {
         //context.fillStyle = "black";
         //context.fill();
         context.lineWidth = 1;
-        context.strokeStyle = "black";
+        context.strokeStyle = "white";
         context.stroke();
         //context.closePath();
 
-        radius = (min / 2147483647) * (topView.width / 2);
+        radius = (min / MAX_DSCAN_KM) * (topView.width / 2);
         context.beginPath();
         context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
         //context.fillStyle = "black";
         //context.fill();
         context.lineWidth = 1;
-        context.strokeStyle = "black";
+        context.strokeStyle = "white";
         context.stroke();
         //context.closePath();
 
-        radius = ((((max - min) / 2) + min) / 2147483647) * (topView.width / 2);
+        radius = ((((max - min) / 2) + min) / MAX_DSCAN_KM) * (topView.width / 2);
         context.beginPath();
         context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
         //context.fillStyle = "black";
         //context.fill();
-        context.lineWidth = (range / 2147483647) * (topView.width / 2);
-        context.strokeStyle = "red";
+        context.lineWidth = (range / MAX_DSCAN_KM) * (topView.width / 2);
+        context.strokeStyle = "#4EB1BA";
         context.stroke();
         //context.closePath();
 
 
         for(var i=5;i<20;i+=5)
         {
-          radius = ((i * AU_KM_FACTOR) / 2147483647) * (topView.width / 2);
+          context.font="12px Arial";
+          context.fillStyle = "#E9E9E9";
+          context.fillText(i,topView.width/2, topView.height / 2 - ((i-2)*AU_KM_FACTOR / MAX_DSCAN_KM) * (topView.width / 2));
+
+          radius = ((i * AU_KM_FACTOR) / MAX_DSCAN_KM) * (topView.width / 2);
           context.beginPath();
           context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
           //context.fillStyle = "black";
@@ -118,7 +148,7 @@ function AuConverterView(AuTextBox, KmTextBox) {
   };
 }
 
-function MinIncreaser(maxText, minText, testText, scanHistory) {
+function MinIncreaser(maxText, minText, testText, history, midpoint) {
   "use strict";
   return function() {
     var max = parseFloat(maxText.value),
@@ -136,11 +166,11 @@ function MinIncreaser(maxText, minText, testText, scanHistory) {
     state = {};
     state.max = max;
     state.min = min;
-    scanHistory.push(state);
+    history.push(state);
   }
 }
 
-function MaxDecreaser(maxText, minText, testText, scanHistory) {
+function MaxDecreaser(maxText, minText, testText, history, midpoint) {
   "use strict";
   return function() {
     var max = parseFloat(maxText.value),
@@ -158,54 +188,54 @@ function MaxDecreaser(maxText, minText, testText, scanHistory) {
     state = {};
     state.max = max;
     state.min = min;
-    scanHistory.push(state);
+    history.push(state);
   }
 }
 
-var AU_KM_FACTOR = 149597871,
-    convertAuToKm,
-    convertKmToAu,
-    increaseMin,
-    decreaseMax,
-    updateRanges,
-    resetSearch,
-    undo;
-
-function Undoer(scanHistory, maxTextBox, minTextBox) {
+function Undoer(history, maxTextBox, minTextBox) {
   return function() {
-    if(scanHistory.length > 0)
-    {
-      var state = scanHistory.pop();
+    var state = history.pop();
 
-      maxTextBox.value = state.max;
-      minTextBox.value = state.min;
+    maxTextBox.value = state.max;
+    minTextBox.value = state.min;
 
-      updateRanges();
-    }
+    updateRanges();
   }
+}
+
+function MidpointSetter(maxTextBox, minTextBox, testText, history, midpoint) {
+	"use strict";
+	return function() {
+		increaseMin = MinIncreaser(maxTextBox, minTextBox, testText, history, midpoint);
+		decreaseMax = MaxDecreaser(maxTextBox, minTextBox, testText, history, midpoint);
+    updateTestpoint = TestpointCalculator(maxTextBox, minTextBox, testText, midpoint);
+	}
 }
 
 function initialize() {
-  "use strict";
-  var kmTextBox = document.getElementById("km"),
-      auTextBox = document.getElementById("au"),
-      maxTextBox = document.getElementById("max"),
-      minTextBox = document.getElementById("min"),
-      testText = document.getElementById("test"),
-      spanText = document.getElementById("span"),
-      topView = document.getElementById("topView");
+	"use strict";
+	var kmTextBox = document.getElementById("km"),
+			auTextBox = document.getElementById("au"),
+			maxTextBox = document.getElementById("max"),
+			minTextBox = document.getElementById("min"),
+			testText = document.getElementById("test"),
+			spanText = document.getElementById("span"),
+			topView = document.getElementById("topView"),
+			history = [];
 
-  var scanHistory = new Array();
-  undo = Undoer(scanHistory, maxTextBox, minTextBox);
+	undo = Undoer(history, maxTextBox, minTextBox);
 
-  convertAuToKm = AuConverterView(auTextBox, kmTextBox);
-  convertKmToAu = KmConverterView(auTextBox, kmTextBox);
+	convertAuToKm = AuConverterView(auTextBox, kmTextBox);
+	convertKmToAu = KmConverterView(auTextBox, kmTextBox);
 
-  increaseMin = MinIncreaser(maxTextBox, minTextBox, testText, scanHistory);
-  decreaseMax = MaxDecreaser(maxTextBox, minTextBox, testText, scanHistory);
+	setMidpoint1d = MidpointSetter(maxTextBox, minTextBox, testText, history, midpoint1d);
+	setMidpoint2d = MidpointSetter(maxTextBox, minTextBox, testText, history, midpoint2d);
+	setMidpoint3d = MidpointSetter(maxTextBox, minTextBox, testText, history, midpoint3d);
 
-  updateRanges = onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView);
-  resetSearch = SearchResetter(maxTextBox, minTextBox);
+	setMidpoint3d();
 
-  resetSearch();
+	updateRanges = onRangeChanged(maxTextBox, minTextBox, testText, spanText, topView, midpoint3d);
+	resetSearch = SearchResetter(maxTextBox, minTextBox);
+
+	resetSearch();
 }
